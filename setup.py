b0eb4a1ba3623cde269cec7562cfca216dd54636
Nathaniel J. Fernandes <njferns020@gmail.com>
2023-12-24 14:49:03 -0600

    updated filename in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DESCRIPTION = 'Python wrapper of R package `iglu` for continuous glucose monitor
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "readme.md").read_text()
+long_description = (this_directory / "README.md").read_text()
 
 # Setting up
 setup(
